018994f73a6bc236f5783aa26195137a914fc261
dev-jv <dev.javierleon@gmail.com>
2020-12-23 16:13:52 -0500

    four

--- app.js
+++ app.js
@@ -1,29 +1,55 @@
-// const argv = require('yargs').argv;
-const argv = require('./config/yargs').argv;
+const argv = require("./config/yargs").argv;
 
-const {crear} = require('./to-do/to-do');
+const {crear} = require("./to-do/to-do");
 
 let comando = argv._[0];
 
-switch( comando ) {
-    case 'crear':
+switch (comando) {
+    case "crear":
         // console.log('Crea una tarea');
-        crear(argv.description)
-            .then(res => console.log(res))
-            .catch(e => console.log(e));
+        crear(argv.description);
         break;
 
-    case 'listar':
-        console.log('Muestra todas las tareas pendientes');
+    case "listar":
+        console.log("Muestra todas las tareas pendientes");
         break;
 
-    case 'actualizar':
-        console.log('Actualiza el estado de una tarea');
+    case "actualizar":
+        console.log("Actualiza el estado de una tarea");
         break;
 
     default:
-        console.log('El comando no es reconocido');
+        console.log("El comando no es reconocido");
 }
+
+// ------------------------------------------------ <> with Promise
+
+// // const argv = require('yargs').argv;
+// const argv = require('./config/yargs').argv;
+//
+// const {crear} = require('./to-do/to-do');
+//
+// let comando = argv._[0];
+//
+// switch( comando ) {
+//     case 'crear':
+//         // console.log('Crea una tarea');
+//         crear(argv.description)
+//             .then(res => console.log(res))
+//             .catch(e => console.log(e));
+//         break;
+//
+//     case 'listar':
+//         console.log('Muestra todas las tareas pendientes');
+//         break;
+//
+//     case 'actualizar':
+//         console.log('Actualiza el estado de una tarea');
+//         break;
+//
+//     default:
+//         console.log('El comando no es reconocido');
+// }
 
 
 
--- to-do/to-do.js
+++ to-do/to-do.js
@@ -1,35 +1,91 @@
-const fs = require('fs');
+const fs = require("fs");
 
 let list = [];
 
-const crear = (description) => {
+const saveDB = () => {
+    let data = JSON.stringify(list);
+    fs.writeFile(`db/data.json`, data, e => {
+        if (e) {
+            throw new Error("No se pudo crear la tarea");
+        } else {
+            console.log(`La tarea fué agregada a data.json`);
+        }
+    });
+};
+
+const actDB = () => {
+    try {
+        list = require("../db/data.json");
+    }catch (e) {
+        lis = []
+    }
+};
+
+const crear = description => {
+    actDB();
+
     let task = {
         description,
         state: false
     };
+
     list.push(task);
 
-    let data = JSON.stringify(list);
+    saveDB();
 
-    return new Promise ((resolve, reject) => {
-        if (description.length < 10) {
-            reject(`La descripción debe contener mas de 10 letras`);
-            return;
-        }
-
-        fs.writeFile(`db/data.json`, data, (e) => {
-            if(e){
-                reject(new Error('No se pudo crear la tarea'));
-            }else{
-                resolve(`La tarea fué agregada a data.json`);
-            }
-        })
-    })
+    // return task;
 };
 
 module.exports = {
     crear
 };
+
+// ------------------------------------------------ <> with Promise
+
+// const fs = require('fs');
+//
+// let list = [];
+//
+// const saveDB = () => {
+//     let data = JSON.stringify(list);
+//
+//     return new Promise((resolve, reject) => {
+//         fs.writeFile(`db/data.json`, data, e => {
+//             if(e){
+//                 reject(new Error('No se pudo crear la tarea'));
+//             }else{
+//                 resolve(`La tarea fué agregada a data.json`);
+//             }
+//         });
+//     });
+// };
+//
+// const actDB = () => {
+//     try {
+//         list = require("../db/data.json");
+//     }catch (e) {
+//         lis = []
+//     }
+// };
+//
+// const crear = description => {
+//     actDB();
+//
+//     let task = {
+//         description,
+//         state: false
+//     };
+//
+//     list.push(task);
+//
+//     saveDB();
+//
+//     return saveDB();
+// };
+//
+// module.exports = {
+//     crear
+// };
 
 
 
